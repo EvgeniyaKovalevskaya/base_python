be3c921f91ab6f0adf369a36967ac7f6153fb13a
EvgeniyaKovalevskaya <janchik7@yahoo.com>
2025-02-01 13:02:35 +0300

    numbers, strings

--- Workbook_les_1.py
+++ Workbook_les_1.py
@@ -1,25 +1,94 @@
-a = 7
-b = a
-print(b)
 
-print(type(a))
-print(type('name'))
+# a = 7
+# b = a
+# print(b)
+#
+# print(type(a))
+# print(type('name'))
+#
+# b = b -3
+# print(b)
+#
+# b *=2
+# print(b)
+#
+# print(divmod(8, 5))
+# print(int(8.89))
+#
+# googol = 10**100
+# print(googol)
+#
+# Функции для работы со строками
+# say = 'A man. \nA cat. \n\tA dog.'
+# print(len('name'))
+#
+# test = 'asdfggfgfgg'
+# print(test[0:6:2])
 
-b = b -3
-print(b)
+# todos = 'get gloves, get mask, give cat, call ambulance'
+# print(todos.split())
+#
+# crypto_list = ['Yeti', 'Golum']
+# crypto_string = ', '.join(crypto_list)
+# print('Found and singing book deals:', crypto_string)
+#
+# poem = '''All that doth flow we cannot liquid name
+# Or else would fire and water be the same;
+# But that is liquid which is moist and wet
+# Fire that property can never get.
+# Then 'tis not cold that doth the fire put out
+# But 'tis the wet that makes it die, no doubt.'''
+#
+# print(poem[:13])
+# print(len(poem))
+# print(poem.startswith('All'))
+# print(poem.endswith('All'))
+#
+# word = 'the'
+# print(poem.find(word))
+# print(poem.rfind(word))
+# print(poem.count(word))
+# print(poem.isalnum())
 
-b *=2
-print(b)
+# setup = 'a duck goes into a bar....'
+# set = setup.strip(".")
+# print(set)
+# set_2 = set.capitalize()
+# print(set_2)
+# set_3 = set_2.upper()
+# print(set_3)
+# print(set_3.center(50))
+# set_4 = set_3.center(100)
+# print(set_4.ljust(200))
+# print(set_3.rjust(40))
+#
+# setup.replace('duck', 'mamooth')
+# print(setup.replace('duck', 'mamooth'))
+# print(setup.replace('a ', 'an ')
 
-print(divmod(8, 5))
-print(int(8.89))
+# #1 Сколько секунд в часе?
+second = 1
+minute = second * 60  # 60 секунд в минуте
+hour = minute * 60   # 60 минут в часе
+print(hour)  # выводит 3600
 
-googol = 10**100
-print(googol)
+seconds_per_hour = hour
+print(seconds_per_hour)
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day)
 
-say = 'A man. \nA cat. \n\tA dog.'
-print(say)
+print(seconds_per_day / seconds_per_hour)
+print(seconds_per_day // seconds_per_hour)
 
-test = 'asdfggfgfgg'
-print(test[0:6:2])
+
+
+
+
+
+
+
+
+
+
+
 

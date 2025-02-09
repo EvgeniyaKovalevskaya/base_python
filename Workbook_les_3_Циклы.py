c7a3c35b35179c577f6f8bf36c1f25eb585ad124
Евгения  Ковалевская <evgeniakovalevskaa@MacBook-Air-2.local>
2025-02-09 16:54:14 +0300

    home

--- Workbook_les_3_Циклы.py
+++ Workbook_les_3_Циклы.py
@@ -101,4 +101,12 @@ number_list = []
 #         print(row, col)
 
 
-rows =
+rows = range(1, 4)
+cols = range(1, 3)
+cells = [(row, col) for row in rows for col in cols]
+for cell in cells:
+    print(cell)
+
+# Распаковка кортежа
+for row, col in cells:
+    print(row, col)

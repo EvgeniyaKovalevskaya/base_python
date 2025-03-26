cf931fd8f4668da1df22b6db2a695f8b4e70779e
Евгения  Ковалевская <evgeniakovalevskaa@MacBook-Air-2.local>
2025-03-26 10:38:13 +0300

    W

--- Stepik_1.py
+++ Stepik_1.py
@@ -68,20 +68,51 @@
 # name, surname = name_input, surname_input
 # print('Имя:', name, 'Фамилия:', surname)
 
-name1 = 'Timur'
-name2 = 'Gvido'
+# name1 = 'Timur'
+# name2 = 'Gvido'
+#
+# print(name1, name2)
+#
+# name1 = name2
+# name2 = name1
+# print(name1, name2)
+#
+# length_side = input()
+# val = int(length_side) ** 3
+# print('Объем =', val)
+# square = 6 * (int(length_side) ** 2)
+# print('Площадь полной поверхности =', square)
 
-print(name1, name2)
+# num1 = int(input())
+# next_num = num1 + 1
+# prev_num = num1 - 1
+# print('Следующее за числом,', num1, 'число:', next_num)
+# print('Для числа', num1, 'предыдущее число:', prev_num)
 
-name1 = name2
-name2 = name1
-print(name1, name2)
+# a = int(input())
+# b = int(input())
+#
+# sum_num = a + b
+# dif = a - b
+# umnozh = a * b
+#
+# print(a, '+', b, '=', sum_num)
+# print(a, '-', b, '=', dif)
+# print(a, '*', b, '=', umnozh)
 
-length_side = input()
-val = int(length_side) ** 3
-print('Объем =', val)
-square = 6 * (int(length_side) ** 2)
-print('Площадь полной поверхности =', square)
 
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+#
+# an = a1 + d * (n - 1)
+#
+# print(an)
 
+x = int(input('Put your number: '))
+x2 = x * 2
+x3 = 3 * x
+x4 = 4 * x
+x5 = 5 * x
 
+print(f'{x}---{x2}---{x3}---{x4}---{x5}')
